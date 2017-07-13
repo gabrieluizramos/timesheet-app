2043899cc056df6fc59bd10e52727585c6e602fd
Gabriel Ramos <gabriel.l.ramos@walmart.com>
2017-07-13 19:45:44 -0300

    Update create database

--- js/database.js
+++ js/database.js
@@ -1,29 +1,39 @@
 var database = (function(){
-  var db = openDatabase('Timesheet', '1.0' , 'Datbase for Timesheet front-end app', 2 * 1024 * 1024 , function(){
+  var db = openDatabase('db_timesheet', '1.0' , 'Datbase for t front-end app', 2 * 1024 * 1024 , function(){
     console.log('Database opened');
   });
 
   (function createTables(db){
-    db.transaction(function(tx){
-      tx.executeSql('INSERT INTO ( CREATE TABLE IF NOT EXISTS tb_meses ( id INTEGER PRIMARY KEY, nm_mes VARCHAR(50) ) VALUES ( "Janeiro" ), ( "Fevereiro" ), ( "Março" ), ( "Abril" ), ( "Maio" ), ( "Junho" ), ( "Julho" ), ( "Agosto" ), ( "Setembro" ), ( "Outubro" ), ( "Novembro" ), ( "Dezembro" );');
+    // creation working
+    // TODO: validate if exists a creation and insert unique query with IF NOT EXISTS statement
+    db.transaction(function ( tx ) {
+      tx.executeSql('CREATE TABLE tb_meses ( id INTEGER PRIMARY KEY, nm_mes VARCHAR(50) );');
+      tx.executeSql('INSERT INTO tb_meses ( nm_mes ) VALUES ( "Janeiro" ), ( "Fevereiro" ), ( "Março" ), ( "Abril" ), ( "Maio" ), ( "Junho" ), ( "Julho" ), ( "Agosto" ), ( "Setembro" ), ( "Outubro" ), ( "Novembro" ), ( "Dezembro" );');
     });
   })(db);
 
-  function _droptable ( table ) {
-     db.transaction(function(tx){
+  function _dropTable ( table ) {
+     db.transaction(function ( tx ) {
       tx.executeSql('DROP TABLE ' + table);
      });
   }
   
 
-  function _executeSQL (query, callback) {
-    db.transaction(function(tx){
+  function _executeQuery (query, callback) {
+    db.transaction(function ( tx ) {
       tx.executeSql(query);
     });
   }
 
+  function _dropDatabase () {
+    db.transaction(function ( tx ) {
+      tx.executeSql('DROP DATABASE db_timesheet');
+    });
+  }
+
   return {
-    executeSQL: _executeSQL,
-    droptable: _droptable
+    executeQuery: _executeQuery,
+    dropTable: _dropTable,
+    dropDatabase: _dropDatabase
   }
 })();
