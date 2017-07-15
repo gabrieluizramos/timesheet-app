d6a0e31435609ee2124ca00f1c1c82ab93abd50f
Gabriel Ramos <gabriel.l.ramos@walmart.com>
2017-07-15 09:01:24 -0300

    Create report table creation

--- js/app.js
+++ js/app.js
@@ -73,7 +73,92 @@ var app = (function ( date , day_output , button_enter , button_lunch , button_r
 
   button_report.onclick = function () {
     var month = _getNow().monthName
-    console.log(database.getMonthData(month));
+    var table = getReportTable(month)
+  }
+
+  // TODO: refactor table functions
+
+  function getReportTableHead (month, monthData){
+
+    var thead = document.createElement('thead');
+    thead.className = 'report-thead'
+    
+    var tr = document.createElement('tr');
+    tr.className = 'report-row';
+    
+    var th = document.createElement('th');
+    th.className = 'report-cell'
+    th.textContent = month;
+
+    tr.appendChild(th);
+    thead.appendChild(tr);
+
+    var headCellsType = monthData[Object.keys(monthData)[0]].horarios;
+    
+    tr = document.createElement('tr');
+    tr.className = 'report-row';
+    
+    for ( var horario in headCellsType ) {
+      th = document.createElement('th');
+      th.className = 'report-cell';
+      th.textContent = horario;
+      tr.appendChild(th);
+    }
+
+    thead.appendChild(tr);
+
+    return thead;
+  }
+
+  function getReportTableBody (monthData) {
+    var tbody = document.createElement('tbody');
+    tbody.className = 'report-body';
+
+    for ( var day in monthData ) {
+      var tr = document.createElement('tr');
+      tr.className = 'report-row';
+
+      var td = document.createElement('td');
+      td.className = 'report-cell';
+      td.textContent = day;
+
+      tr.appendChild(td);
+
+      for ( var conteudo in monthData[day] ) {
+        td = document.createElement('td');
+        td.className = 'report-cell';
+
+        if ( typeof monthData[day][conteudo] !== 'string' ) {
+          for ( var horario in monthData[day][conteudo] ) {
+
+          }
+        }
+        else {
+          td.textContent = monthData[day][conteudo];
+        }
+      }
+
+      tr.appendChild(td);
+
+      tbody.appendChild(tr);
+    }
+
+    return tbody;
+  }
+
+  function getReportTable (month) {
+    var monthData = database.getMonthData(month);
+    
+    var table = document.createElement('table');
+    table.className = 'report-table';
+
+    table.appendChild(getReportTableHead(month, monthData));
+    table.appendChild(getReportTableBody(monthData));
+
+    console.log(table);
+
+
+    
   }
 
 })(
